ec262d008621161effe2786b34416bb65a12f852
Vitaly Bibik <wital2010@gmail.com>
2021-03-28 14:24:56 +0300

    test build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {compose, createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {rootReducer} from './redux/rootReducer'
+import {forbiddenWordsMiddleware} from './redux/middlware'
+import {sagaWatcher} from './redux/sagas'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk, forbiddenWordsMiddleware, saga
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
+))
+
+saga.run(sagaWatcher)
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+render(app, document.getElementById('root'));
+
+
